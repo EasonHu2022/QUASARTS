cf0619fa0c0b89dd6d6878b40580e0ced4ba6b44
pm17s2b <pm17s2b@leeds.ac.uk>
2022-03-10 15:12:49 +0000

    Static raycasting test in PhysicsManager::start().

--- project/QUASARTS/Engine/src/PhysicsManager.cpp
+++ project/QUASARTS/Engine/src/PhysicsManager.cpp
@@ -42,13 +42,17 @@ void PhysicsManager::init()
 	btCollisionObject* testCollisionObject = new btCollisionObject();
 
 	// Set object's collision shape.
-	btCollisionShape* testCollisionShape = new btSphereShape( 1.f ); // Test with sphere shape: radius = 1.
+	btCollisionShape* testCollisionShape = new btSphereShape( 2.66666f ); // Test with sphere shape: radius = 1.
 	collisionShapes.push_back( testCollisionShape ); // Store collision shape - can be re-used!
 	testCollisionObject->setCollisionShape( testCollisionShape );
 
 	// Set object's world transform.
-	btQuaternion orn = btQuaternion( 15.f, -99.99999f, 0 ); // Constructing from Euler angles (yawZ, pitchY, rollX).
-	btVector3 pos = btVector3( 1.33333f, -3.f, 0 );
+	float yaw, pitch, roll;
+	yaw = 15.f;
+	pitch = -99.99999f;
+	roll = 0;
+	btQuaternion orn = btQuaternion( yaw * SIMD_RADS_PER_DEG, pitch * SIMD_RADS_PER_DEG, roll * SIMD_RADS_PER_DEG ); // Constructing from Euler angles (yawZ, pitchY, rollX).
+	btVector3 pos = btVector3( 60.5f, -109.77777f, 0 );
 	btTransform testTransform = btTransform( orn, pos );
 	testCollisionObject->setWorldTransform( testTransform ); // Apply transform to object.
 
@@ -59,18 +63,21 @@ void PhysicsManager::init()
 
 int PhysicsManager::start()
 {
-	QDEBUG("Test for PhysicsManager start():\n");
+	QDEBUG( "Tests in PhysicsManager start():" );
 
-	char msg[64];
-	snprintf(msg, 64, "Number of collision objects in collision world: %d\n", collisionWorld->getNumCollisionObjects());
-	QDEBUG(msg);
+	char msg[128];
+
+	QDEBUG( "------------------------------" );
+	snprintf( msg, 128, "Number of collision objects in collision world: %d", collisionWorld->getNumCollisionObjects() );
+	QDEBUG( msg );
+
 
 	// Check persistence of collision world state since initialisation.
 	
-	for (int i = 0; i < collisionWorld->getNumCollisionObjects(); ++i)
+	for ( int i = 0; i < collisionWorld->getNumCollisionObjects(); ++i )
 	{
 
-		snprintf( msg, 64, "Collision object %i", i );
+		snprintf( msg, 128, "Collision object %i", i );
 		QDEBUG( msg );
 
 		btCollisionObject* obj = collisionWorld->getCollisionObjectArray()[i];
@@ -80,12 +87,64 @@ int PhysicsManager::start()
 		btQuaternion orn = trf.getRotation();
 		float yaw, pitch, roll;
 		orn.getEulerZYX(yaw, pitch, roll);
-		snprintf( msg, 64, "World Orientation: (%5.5f, %5.5f, %5.5f)", yaw, pitch, roll );
+		snprintf( msg, 128, "World Orientation (rad): (%5.5f, %5.5f, %5.5f)", yaw, pitch, roll );
+		QDEBUG( msg );
+		snprintf( msg, 128, "World Orientation (deg): (%5.5f, %5.5f, %5.5f)", yaw * SIMD_DEGS_PER_RAD, pitch * SIMD_DEGS_PER_RAD, roll * SIMD_DEGS_PER_RAD );
 		QDEBUG( msg );
 
 		btVector3 pos = trf.getOrigin();
-		snprintf( msg, 64, "World Position: (%5.5f, %5.5f, %5.5f)", pos.getX(), pos.getY(), pos.getZ() );
+		snprintf( msg, 128, "World Position: (%5.5f, %5.5f, %5.5f)", pos.getX(), pos.getY(), pos.getZ() );
 		QDEBUG( msg );
+
+		btSphereShape* collisionSphere = dynamic_cast<btSphereShape*>( obj->getCollisionShape() );
+		if ( nullptr != collisionSphere )
+		{
+			snprintf( msg, 128, "Collision shape: Sphere, radius: %5.5f", collisionSphere->getRadius() );
+			QDEBUG( msg );
+		}
+
+	}
+
+
+	// Test raycasting against collision objects.
+	QDEBUG( "------------------------------" ); 
+	QDEBUG( "Static raycasting tests, closest hits:" );
+
+	// Raycast from world origin to each collision object origin.
+	btVector3 worldOrigin(0,0,0);
+	for ( int i = 0; i < collisionWorld->getNumCollisionObjects(); ++i )
+	{
+		btCollisionObject* obj = collisionWorld->getCollisionObjectArray()[i];
+		btVector3 pos = obj->getWorldTransform().getOrigin();
+
+		// Set up raycast result callback for closest hit.
+		btCollisionWorld::ClosestRayResultCallback closestRaycastResult( worldOrigin, pos );
+		// closestRaycastResult.m_flags |= btTriangleRaycastCallback::kF_KeepUnflippedNormal;
+		// kF_UseGjkConvexRaytest flag is enabled by default
+		// allRaycastResults.m_flags |= btTriangleRaycastCallback::kF_UseSubSimplexConvexCastRaytest; // faster than default algorithm but more approximate
+
+		// Cast ray.
+		collisionWorld->rayTest( worldOrigin, pos, closestRaycastResult );
+
+		// Print results.
+		if ( closestRaycastResult.hasHit() )
+		{
+			btVector3 hitPoint = closestRaycastResult.m_hitPointWorld;
+			snprintf(msg, 128, "Collision object %i hit at world position: (%5.5f, %5.5f, %5.5f)", i, hitPoint.getX(), hitPoint.getY(), hitPoint.getZ() );
+			QDEBUG( msg );
+
+			btSphereShape* collisionSphere = dynamic_cast<btSphereShape*>(obj->getCollisionShape());
+			if (nullptr != collisionSphere)
+			{
+				float objClosestPoint = pos.length() - collisionSphere->getRadius();
+				snprintf( msg, 128, "Object closest point: %f, hit distance: %f, difference: %f", objClosestPoint, hitPoint.length(), objClosestPoint - hitPoint.length());
+				QDEBUG( msg );
+			}
+		}
+		else
+		{
+			QDEBUG( "Collision object %i not hit" );
+		}
 	}
 
 	return 0;
