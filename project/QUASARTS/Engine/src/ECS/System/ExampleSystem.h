5e0bae0ab321d44c1c778f7d04f7f7ca2dc5cd2e
Nell <nell@mad-cat.co.uk>
2022-04-12 13:35:49 +0100

    >:|

--- project/QUASARTS/Engine/src/ECS/System/ExampleSystem.h
+++ project/QUASARTS/Engine/src/ECS/System/ExampleSystem.h
@@ -10,17 +10,63 @@ namespace Engine {
         private:
         // Singleton:
         static ExampleSystem *instance;
-        ExampleSystem();
+        ExampleSystem() {
+            // Set the component mask:
+            quasarts_component_mask *mask = get_component_mask();
+            mask->mask = 0;
+            mask->mask += (uint64_t)1 << COMPONENT_TRANSFORM;
+        }
 
         public:
-        static ExampleSystem *Instance();
+        static ExampleSystem *Instance() {
+            if (nullptr == instance) {
+			    instance = new ExampleSystem();
+            }
+		    return instance;
+        }
         ~ExampleSystem();
 
-        // Functions inherited from IManager:
-        void init();
-		int start();
-		void update();
-		int stop();
-		void release();
+        void init() {
+
+        }
+
+        int start() {
+            return 0;
+        }
+
+        // Update function:
+        void update() {
+            // Get the manager:
+            ECSManager *active_manager = get_manager();
+
+            // Get the entity ID mask:
+            quasarts_entity_ID_mask *entities = get_entity_ID_mask();
+
+            // Increment the transform position:
+            TransformComponent transform;
+            for (int i = 0; i < MAX_ENTITIES; i++) {
+                if (entities->mask[i] == 1) {
+                    // The entity is valid for the System:
+                    transform = active_manager->get_component
+                        <TransformComponent>(i, COMPONENT_TRANSFORM);
+
+                    // Update the component and send it back:
+                    transform.x += 0.1;
+                    transform.y += 0.1;
+                    transform.z += 0.1;
+
+                    active_manager->replace_component<TransformComponent>
+                        (i, COMPONENT_TRANSFORM, transform);
+                }
+            }
+        }
+
+        int stop() {
+            return 0;
+        }
+
+        void release() {
+
+        }
     };
 }
