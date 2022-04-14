b863d186c0af78a8e0bad99a27010aeb04832911
Mario <mking1999@abv.bg>
2022-04-14 14:42:34 +0100

    we've finished the hierarchy

--- project/QUASARTS/Editor/src/GuiViews/MenuBarView.h
+++ project/QUASARTS/Editor/src/GuiViews/MenuBarView.h
@@ -9,12 +9,16 @@ public:
 	bool new_project;
 	bool new_scene;
 	bool new_script;
+	bool new_entity;
+	bool new_child;
 	std::string folder_path;
 	static std::string OpenFileDialogue();
 	static std::string OpenFolderDialogue();
 	void newProject();
 	void newScene();
 	void newScript();
+	void newEntity();
+	void newChild();
 public:
 	void on_add() override;
 	void on_gui() override;
