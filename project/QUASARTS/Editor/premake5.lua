20cfe4bc59d715f65912c28d0622322bf771896d
Nell <nell@mad-cat.co.uk>
2022-05-11 14:09:42 +0100

    Fix for OpenAL library in premakes.

--- project/QUASARTS/Editor/premake5.lua
+++ project/QUASARTS/Editor/premake5.lua
@@ -4,18 +4,7 @@ project "Editor"
 	
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("%{wks.location}/bin_obj/" .. outputdir .. "/%{prj.name}")
-	
-	postbuildcommands { 
-		"powershell Copy-Item '"..rootdir.."/ThirdParty/OpenAL/libs/Win32/OpenAL32.dll' '"..dir.."/Editor/'".." -recurse -Force",
-		"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/vorbisenc.dll' '"..dir.."/Editor/'".." -recurse -Force",
-		"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/FLAC.dll' '"..dir.."/Editor/'".." -recurse -Force",
-		"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/ogg.dll' '"..dir.."/Editor/'".." -recurse -Force",
-		"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/opus.dll' '"..dir.."/Editor/'".." -recurse -Force",
-		"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/vorbis.dll' '"..dir.."/Editor/'".." -recurse -Force",
-		"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/sndfile.dll' '"..dir.."/Editor/'".." -recurse -Force"
-	}
-	postbuildmessage "copying thirdparty"
-	
+
 	files
 	{
 		"src/**.h",
@@ -26,25 +15,18 @@ project "Editor"
 		"../ThirdParty/glm/glm/*.hpp",
 		"../ThirdParty/glm/glm/*.inl",
 		"../ThirdParty/glm/glm/*/*.hpp",
-		"../ThirdParty/glm/glm/*/*.inl",
-	}
-	
-	libdirs
-	{
-		"../ThirdParty/OpenAL/libs/Win32",
-		"../ThirdParty/sndfile/libs/Win32"
+		"../ThirdParty/glm/glm/*/*.inl"
 	}
 	
 	links
 	{
 		"Engine",
-		"OpenAL32",
 		"sndfile",
 		"vorbisenc",
 		"FLAC",
 		"ogg",
 		"opus",
-		"vorbis",
+		"vorbis"
 	}
 
 	includedirs
@@ -67,28 +49,66 @@ project "Editor"
 		"../ThirdParty/tiny_obj_loader",
 		"../ThirdParty/glm",
 		"../ThirdParty/OpenAL/include",
-		"../ThirdParty/sndfile/include",
+		"../ThirdParty/sndfile/include"
 	}
-
+	
 	filter "system:windows"
 		systemversion "latest"
 		staticruntime "Off"
 		defines
 		{
 			"_UNICODE",
-            "UNICODE",
+			"UNICODE",
 			"_CONSOLE",
 			"GLFW_INCLUDE_NONE",
-			"QS_WINDOWS",
+			"QS_WINDOWS"
 		}
+		links
+		{
+			"OpenAL32"
+		}
+		libdirs
+		{
+			"../ThirdParty/OpenAL/libs/Win32",
+			"../ThirdParty/sndfile/libs/Win32"
+		}
+		postbuildcommands { 
+			"powershell Copy-Item '"..rootdir.."/ThirdParty/OpenAL/libs/Win32/OpenAL32.dll' '"..dir.."/Editor/'".." -recurse -Force",
+			"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/vorbisenc.dll' '"..dir.."/Editor/'".." -recurse -Force",
+			"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/FLAC.dll' '"..dir.."/Editor/'".." -recurse -Force",
+			"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/ogg.dll' '"..dir.."/Editor/'".." -recurse -Force",
+			"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/opus.dll' '"..dir.."/Editor/'".." -recurse -Force",
+			"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/vorbis.dll' '"..dir.."/Editor/'".." -recurse -Force",
+			"powershell Copy-Item '"..rootdir.."/ThirdParty/sndfile/libs/Win32/sndfile.dll' '"..dir.."/Editor/'".." -recurse -Force"
+		}
+		postbuildmessage "copying thirdparty"
 
 	filter "system:linux"
 		systemversion "latest"
 		staticruntime "Off"
 		defines
 		{
-			"GLFW_INCLUDE_NONE",
-		}		
+			"GLFW_INCLUDE_NONE"
+		}
+		links
+		{
+			"openal"
+		}
+		libdirs
+		{
+			"../ThirdParty/OpenAL/libs/Linux",
+			"../ThirdParty/sndfile/libs/Linux"
+		}
+		postbuildcommands { 
+			"cp -rf '"..rootdir.."/ThirdParty/OpenAL/libs/Linux/OpenAL.so' '"..dir.."/Editor/OpenAL.so'",
+			"cp -rf '"..rootdir.."/ThirdParty/sndfile/libs/Linux/vorbisenc.a' '"..dir.."/Editor/vorbisenc.a'",
+			"cp -rf '"..rootdir.."/ThirdParty/sndfile/libs/Linux/FLAC.a' '"..dir.."/Editor/FLAC.a'",
+			"cp -rf '"..rootdir.."/ThirdParty/sndfile/libs/Linux/ogg.a' '"..dir.."/Editor/ogg.a'",
+			"cp -rf '"..rootdir.."/ThirdParty/sndfile/libs/Linux/opus.a' '"..dir.."/Editor/opus.a'",
+			"cp -rf '"..rootdir.."/ThirdParty/sndfile/libs/Linux/vorbis.a' '"..dir.."/Editor/vorbis.a'",
+			"cp -rf '"..rootdir.."/ThirdParty/sndfile/libs/Linux/sndfile.a' '"..dir.."/Editor/sndfile.a'"
+		}
+		postbuildmessage "copying thirdparty"	
 	
 	filter "configurations:Debug"
 		runtime "Debug"
