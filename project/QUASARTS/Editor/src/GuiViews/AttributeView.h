5f5eb0ef3b6a7e4214638946343927f5e720981a
HuYiZhou <1505810939@qq.com>
2022-05-12 12:13:46 +0100

    attribute window

--- project/QUASARTS/Editor/src/GuiViews/AttributeView.h
+++ project/QUASARTS/Editor/src/GuiViews/AttributeView.h
@@ -2,7 +2,10 @@
 #include "QuasartsEngine.h"
 #include <string>
 #include "GuiViews/GuiView.h"
-
+#include <memory>
+#include <map>
+#include <unordered_map>
+#include <functional>
 
 class AttributeView : public GuiView
 {
@@ -13,14 +16,21 @@ public:
 	void on_remove() override;
 
 	bool game = false;
-
 private:
+	size_t current_entity_id;
 	void show_transform();
 	void change_transform(Engine::TransformComponent*, float[], float[], float[]);
 	void show_mesh();
-	void show_collision_sphere();
-	void show_material();
-	void show_lighting();
-	void show_script();
 
+	
+	/********************** drawfunctions ************************************/
+	void draw_property(std::string name, glm::vec2 target);
+	void draw_property(std::string name, glm::vec3 target);
+	void draw_property(std::string name, glm::vec4 target);
+	void draw_property(std::string name, std::string target);
+	void draw_property(std::string name, float target);
+	/********************** drawfunctions ************************************/
+
+
+	
 };
