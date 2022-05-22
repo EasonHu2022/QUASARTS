d139bbff62a4338ee839587bbdd2e4c2fb3a3081
Nell <nell@mad-cat.co.uk>
2022-05-22 18:15:58 +0100

    Allowed relative paths in saving and loading.

--- project/QUASARTS/Engine/src/ECS/Component/MaterialComponent.h
+++ project/QUASARTS/Engine/src/ECS/Component/MaterialComponent.h
@@ -27,20 +27,39 @@ namespace Engine {
         float shininess;
 
         // Don't bother with the pointer, just the path:
+        std::string texturePath;
         inStream >> material.path
             >> ambient.r >> ambient.g >> ambient.b
             >> diffuse.r >> diffuse.g >> diffuse.b
             >> specular.r >> specular.g >> specular.b
             >> emission.r >> emission.g >> emission.b
             >> emissiveRange
-            >> shininess;
+            >> shininess >> texturePath;
 
         // Get default engine assets path:
         auto path = FileModule::Instance()->get_internal_assets_path();
+
+        if (material.path == "None")
+        {
+            material.path = "";
+        }
+        else
+        {
+            material.path = path + material.path;
+        }
+
+        if (texturePath == "None")
+        {
+            texturePath = path + "Texture/white.png";
+        }
+        else
+        {
+            texturePath = path + texturePath;
+        }
+
         std::string vshPath = path + "Shader/DefaultShader.vsh";
         std::string fshPath = path + "Shader/DefaultShader.fsh";
         std::string gshPth = "";
-        std::string texturePath = path + "Texture/white.png";
         material.material = new Engine::Material(vshPath, fshPath, gshPth, texturePath);
 
         material.material->ambient = ambient;
@@ -56,15 +75,38 @@ namespace Engine {
     // Output stream operator:
     inline std::ostream & operator << (std::ostream &outStream, const MaterialComponent &material) {
         // Don't bother with the pointer, just the path:
-        outStream << material.path << " "
+        if (material.path.empty())
+        {
+            outStream << "None";
+        }
+        else
+        {
+            auto path = FileModule::Instance()->get_internal_assets_path();
+            std::size_t pathLength = path.size();
+            std::string temp = (material.path).substr(pathLength);
+            outStream << temp;
+        }
+
+        outStream << " "
             << material.material->ambient.r << " " << material.material->ambient.g << " " << material.material->ambient.b << " "
             << material.material->diffuse.r << " " << material.material->diffuse.g << " " << material.material->diffuse.b << " "
             << material.material->specular.r << " " << material.material->specular.g << " " << material.material->specular.b << " "
             << material.material->emission.r << " " << material.material->emission.g << " " << material.material->emission.b << " "
             << material.material->emissiveRange << " "
-            << material.material->shininess;
+            << material.material->shininess << " ";
 
-        
+        if (material.material->texturePath.empty())
+        {
+            outStream << "None";
+        }
+        else
+        {
+            auto path = FileModule::Instance()->get_internal_assets_path();
+            std::size_t pathLength = path.size();
+            std::string temp = (material.material->texturePath).substr(pathLength);
+            outStream << temp;
+        }
+
         return outStream;
     }
 }
--- project/QUASARTS/Engine/src/ECS/Component/MeshComponent.h
+++ project/QUASARTS/Engine/src/ECS/Component/MeshComponent.h
@@ -5,6 +5,7 @@
 
 // Local includes:
 #include "ECS/ECS-Common.h"
+#include "ResourceManager/FileModule.h"
 
 namespace Engine {
     struct QS_API MeshComponent {
@@ -21,12 +22,33 @@ namespace Engine {
     inline std::istream & operator >> (std::istream &inStream, MeshComponent &mesh) {
         inStream >> mesh.resId >> mesh.path;
 
+        if (mesh.path == "None")
+        {
+            mesh.path = "";
+        }
+        else
+        {
+            auto path = FileModule::Instance()->get_internal_assets_path();
+            mesh.path = path + mesh.path;
+        }
+
         return inStream;
     }
             
     // Output stream operator:
     inline std::ostream & operator << (std::ostream &outStream, const MeshComponent &mesh) {
-        outStream << mesh.resId << " " << mesh.path;
+        outStream << mesh.resId << " ";
+        if (mesh.path.empty())
+        {
+            outStream << "None";
+        }
+        else
+        {
+            auto path = FileModule::Instance()->get_internal_assets_path();
+            std::size_t pathLength = path.size();
+            std::string temp = (mesh.path).substr(pathLength);
+            outStream << temp;
+        }
         
         return outStream;
     }
